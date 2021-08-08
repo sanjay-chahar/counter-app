2deacde39087b8c322dd9bfddc90132937334b2c
root <root@ip-172-31-19-175.us-east-2.compute.internal>
2021-08-08 10:47:33 +0000

    change from visit to visitor

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ client.set('visits', 0);
 
 app.get('/', (req, res) => {
   client.get('visits', (err, visits) => {
-    res.send('Number of visits ' + visits);
+    res.send('Number of visitor to this website ' + visits);
     client.set('visits', parseInt(visits) + 1);
   });
 });
